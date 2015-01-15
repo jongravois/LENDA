e8e5327dbc502415a6fa8eb998442eb71ce46a70
Jon Gravois <jongravois@gmail.com>
2015-01-15 08:20:34 -0600

    Added .run() config to fix page scrolling issue

--- public/angular/arm.js
+++ public/angular/arm.js
@@ -1,12 +1,11 @@
 (function(){
     'use strict';
     angular.module('ARM', ['ui.router', 'ui.bootstrap', 'ui.utils', 'ngSanitize', 'ngAnimate', 'ngResource', 'angularMoment', 'angular-loading-bar', 'toastr', 'ngGrid'])
-      /*.run(function($rootScope) {
-        function message(to, toP, from, fromP) { return from.name  + angular.toJson(fromP) + " -> " + to.name + angular.toJson(toP); }
-        $rootScope.$on("$stateChangeStart", function(evt, to, toP, from, fromP) { console.log("Start:   " + message(to, toP, from, fromP)); });
-        $rootScope.$on("$stateChangeSuccess", function(evt, to, toP, from, fromP) { console.log("Success: " + message(to, toP, from, fromP)); });
-        $rootScope.$on("$stateChangeError", function(evt, to, toP, from, fromP, err) { console.log("Error:   " + message(to, toP, from, fromP), err); });
-      })*/
+      .run(function($rootScope) {
+        $rootScope.$on("$routeChangeSuccess", function(){
+          window.scrollTo(0,0);
+        });
+      })
       .config(function(toastrConfig) {
         angular.extend(toastrConfig, {
           closeButton: true
