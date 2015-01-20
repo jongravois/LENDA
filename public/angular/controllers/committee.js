40faf456cec34bbbda98de1404be127d00d379a2
Jon Gravois <jongravois@gmail.com>
2015-01-20 07:06:34 -0600

    Reinstantiated committee in committee.js

--- public/angular/controllers/committee.js
+++ public/angular/controllers/committee.js
@@ -1,6 +1,21 @@
 (function(){
   'use strict';
   angular
-    .module('ARM')
-    .controller('CommitteeController', function($scope){});
+      .module('ARM')
+      .controller('CommitteeController', CommitteeController);
+  
+      CommitteeController.$inject = ['$scope', '$state', '$stateParams', 'AppFactory', 'LoansFactory'];
+  
+      function CommitteeController(
+          $scope,
+          $state,
+          $stateParams,
+          AppFactory,
+          LoansFactory
+      ){
+        LoansFactory.getCommittee($stateParams.loanID)
+          .then(function success(rsp){
+              $scope.committee = rsp.data.data;
+          });
+      } // end function
 })();
