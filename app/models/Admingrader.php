b12960996e3343addf2f0a75a2acd7690c353856
Jon Gravois <jongravois@gmail.com>
2015-01-24 09:25:22 -0600

    minor change to test

--- app/models/Admingrader.php
+++ app/models/Admingrader.php
@@ -1,6 +1,8 @@
 <?php
 
-class Admingrader extends \Eloquent {
+use Eloquent;
+
+class Admingrader extends Eloquent {
 	protected $table = 'admingrader';
 	public $timestamps = false;
 	protected $fillable = ['grade', 'debt2asset', 'current_ratio', 'working_capital', 'borrowing_capacity', 'years_farming', 'credit_score', 'cpa_financials', 'bankruptcy', 'judgement', 'all_max_loan', 'ag_pro_max_loan', 'capital_bridge_max_loan', 'ag_vest_max_loan', 'ag_pro_max_rate', 'capital_bridge_max_rate', 'ag_vest_max_rate'];
--- app/models/Affiliate.php
+++ app/models/Affiliate.php
@@ -1,5 +1,7 @@
 <?php
 
-class Affiliate extends \Eloquent {
+use Eloquent;
+
+class Affiliate extends Eloquent {
 	protected $fillable = ['loan_id', 'entity_name', 'percent_owned'];
 }
