e81e846a844b90132da53b56778491b14e9e7a20
Jon Gravois <jongravois@gmail.com>
2015-02-18 10:20:51 -0600

    Added Grade and Credit Score to sidebar as requested

--- public/angular/controllers/committee.js
+++ public/angular/controllers/committee.js
@@ -4,15 +4,14 @@
       .module('ARM')
       .controller('CommitteeController', CommitteeController);
   
-      CommitteeController.$inject = ['$scope', '$state', '$stateParams', 'AppFactory', 'LoansFactory'];
+      CommitteeController.$inject = ['$scope', '$state', '$stateParams', 'Loan', 'AppFactory', 'LoansFactory'];
   
       function CommitteeController(
-          $scope,
-          $state,
-          $stateParams,
-          AppFactory,
-          LoansFactory
+          $scope, $state, $stateParams,
+          Loan, AppFactory, LoansFactory
       ){
+        $scope.loan = $scope.loan || Loan.data.data[0];
+
         LoansFactory.getCommittee($stateParams.loanID)
           .then(function success(rsp){
               $scope.committee = rsp.data.data;
