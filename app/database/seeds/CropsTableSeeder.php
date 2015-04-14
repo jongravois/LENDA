b6739e194c75db0c1562914a7ad59c3364e0b8e6
Jon Gravois <jongravois@gmail.com>
2015-04-14 09:21:06 -0500

    Added FAC Beans

--- app/database/seeds/CropsTableSeeder.php
+++ app/database/seeds/CropsTableSeeder.php
@@ -9,9 +9,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 750,
 			'measurement' => 'bu',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 5.11,
-      'arm_default_ins_price' => 4.92,
-      'arm_default_yield' => 139
+            'arm_default_price' => 5.11,
+            'arm_default_ins_price' => 4.92,
+            'arm_default_yield' => 139
 		]);
 
 		Crop::create([
@@ -19,19 +19,29 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 450,
 			'measurement' => 'bu',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 11.00,
-      'arm_default_ins_price' => 9.70,
-      'arm_default_yield' => 28
+            'arm_default_price' => 11.00,
+            'arm_default_ins_price' => 9.70,
+            'arm_default_yield' => 28
 		]);
 
-		Crop::create([
+        Crop::create([
+            'crop' => 'FAC Beans',
+            'tea' => 450,
+            'measurement' => 'bu',
+            'rebate_measurement' => 'bu',
+            'arm_default_price' => 11.00,
+            'arm_default_ins_price' => 9.70,
+            'arm_default_yield' => 28
+        ]);
+
+        Crop::create([
 			'crop' => 'sorghum',
 			'tea' => 375,
 			'measurement' => 'bu',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 4.21,
-      'arm_default_ins_price' => 4.00,
-      'arm_default_yield' => 55
+            'arm_default_price' => 4.21,
+            'arm_default_ins_price' => 4.00,
+            'arm_default_yield' => 55
 		]);
 
 		Crop::create([
@@ -39,9 +49,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 360,
 			'measurement' => 'bu',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 6.64,
-      'arm_default_ins_price' => 5.75,
-      'arm_default_yield' => 48
+            'arm_default_price' => 6.64,
+            'arm_default_ins_price' => 5.75,
+            'arm_default_yield' => 48
 		]);
 
 		Crop::create([
@@ -49,9 +59,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 540,
 			'measurement' => 'lb',
 			'rebate_measurement' => 'lb',
-      'arm_default_price' => 93,
-      'arm_default_ins_price' => 90,
-      'arm_default_yield' => 649
+            'arm_default_price' => 93,
+            'arm_default_ins_price' => 90,
+            'arm_default_yield' => 649
 		]);
 
 		Crop::create([
@@ -59,9 +69,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 750,
 			'measurement' => 'lb',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 0.135,
-      'arm_default_ins_price' => 0.14,
-      'arm_default_yield' => 5820
+            'arm_default_price' => 0.135,
+            'arm_default_ins_price' => 0.14,
+            'arm_default_yield' => 5820
 		]);
 
 		Crop::create([
@@ -69,9 +79,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 750,
 			'measurement' => 'bu',
 			'rebate_measurement' => 'bu',
-      'arm_default_price' => 2.3,
-      'arm_default_ins_price' => 2.8,
-      'arm_default_yield' => 3000
+            'arm_default_price' => 2.3,
+            'arm_default_ins_price' => 2.8,
+            'arm_default_yield' => 3000
 		]);
 
 		Crop::create([
@@ -79,9 +89,9 @@ class CropsTableSeeder extends Seeder{
 			'tea' => 750,
 			'measurement' => 'ton',
 			'rebate_measurement' => 'ton',
-      'arm_default_price' => 0.28,
-      'arm_default_ins_price' => 0.16,
-      'arm_default_yield' => 5133
+            'arm_default_price' => 0.28,
+            'arm_default_ins_price' => 0.16,
+            'arm_default_yield' => 5133
 		]);
 	}
 
