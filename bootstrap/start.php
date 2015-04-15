013bb2c28aa664b643eafeed275808875452ff4a
Jon Gravois <jongravois@gmail.com>
2015-04-15 11:07:59 -0500

    Refactored environment file

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,7 +3,16 @@
 $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment( function () {
-    return getenv('APP_ENV') ?: 'local';
+
+  // Defined in the server configuration
+  if ( isset( $_SERVER['APP_ENVIRONMENT'] ) ) {
+    return $_SERVER['APP_ENVIRONMENT'];
+
+    // Look for ./environment.php
+  } elseif ( file_exists( __DIR__ . '/environment.php' ) ) {
+    return include __DIR__ . '/environment.php';
+  }
+
 });
 
 $app->bindInstallPaths(require __DIR__.'/paths.php');
