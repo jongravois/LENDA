8b2f52672d09f94bfeb10109c61aed4cc0f9d7c9
Jon Gravois <jongravois@gmail.com>
2015-04-15 08:55:08 -0500

    Removed ignored files

--- public/angular/constants/constants.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular
-        .module('ARM')
-        .constant('_', window._)
-        .constant('API_URL', 'http://www.lenda.local:6500/api')
-        .constant('FILE_URL', 'http://lenda.app/files_loans/')
-        .constant('LEGAL_NAME', 'Ag Resource Management');
-})();
