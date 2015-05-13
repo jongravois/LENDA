33e93c62e496a4f2516cce50aba248f0dead468d
Jon Gravois <jongravois@gmail.com>
2015-05-13 13:56:33 -0500

    changes to prerequisites

--- public/angular/_constants/constants.js
+++ public/angular/_constants/constants.js
@@ -4,6 +4,6 @@
         .module('ARM')
         .constant('_', window._)
         .constant('API_URL', 'http://www.lenda.local/api')
-        .constant('FILE_URL', 'http://lenda.app/files_loans/')
+        .constant('FILE_URL', 'http://www.lenda.local/files_loans/')
         .constant('LEGAL_NAME', 'Ag Resource Management');
 })();
