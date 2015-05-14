76fbec39f33eb60ecc22d1bd8ab02479df149d92
Jon Gravois <jongravois@gmail.com>
2015-05-13 19:25:02 -0500

    Refactored AppFactory

--- public/angular/loans/prereqs/prerequisites.ctrl.js
+++ public/angular/loans/prereqs/prerequisites.ctrl.js
@@ -7,17 +7,21 @@
     PrerequisitesController.$inject = ['$scope', '$state', '$stateParams', 'AppFactory', 'LoansFactory'];
 
     function PrerequisitesController($scope, $state, $stateParams,AppFactory, LoansFactory) {
-        var curr = $state.current.url;
-        var currScreen = curr.substring(1, curr.length);
-        $scope.newapplication = $state.current.data.newapplication;
+        activate();
 
-        if ($scope.newapplication && $scope.screens) {
-            angular.forEach($scope.screens, function (obj) {
-                if (obj.screen === currScreen) {
-                    obj.status = 1;
-                }
-            });
-        }// end if
+        function activate() {
+            var curr = $state.current.url;
+            var currScreen = curr.substring(1, curr.length);
+            $scope.newapplication = $state.current.data.newapplication;
+
+            if ($scope.newapplication && $scope.screens) {
+                angular.forEach($scope.screens, function (obj) {
+                    if (obj.screen === currScreen) {
+                        obj.status = 1;
+                    }
+                });
+            }// end if
+        } // end activate
 
         $scope.files = [];
 
