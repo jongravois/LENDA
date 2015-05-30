b043903b247eecf196dda6a120ec6ce1987d4745
Jon Gravois <jongravois@gmail.com>
2015-05-29 19:19:11 -0500

    Fixed toolstip for status

--- public/angular/_directives/loan.status.directive.js
+++ public/angular/_directives/loan.status.directive.js
@@ -8,12 +8,12 @@
 
     function LoanStatusIconDirective($compile) {
         var templateMap = {
-            '1': '<span class="staticon glyphicon glyphicon-wrench" tooltip="In_Progress" style="color: #009;"></span>',
-            '2': '<span class="staticon glyphicon glyphicon-ok" tooltip="Paid" style="color: #090;"></span>',
-            '3': '<span class="staticon glyphicon glyphicon-share-alt" tooltip="Recommended"></span>',
-            '4': '<span class="staticon glyphicon glyphicon-thumbs-up" tooltip="Approved"></span>',
-            '5': '<span class="staticon glyphicon glyphicon-thumbs-down" tooltip="Denied" style="color: #900;"></span>',
-            'default': '<span class="staticon glyphicon glyphicon-wrench" tooltip="In_Progress" style="color: #009;"></span>'
+            '1': '<span class="staticon glyphicon glyphicon-wrench" tooltip="In_Progress" tooltip-append-to-body="true" tooltip-placement="bottom" style="color: #009;"></span>',
+            '2': '<span class="staticon glyphicon glyphicon-ok" tooltip="Paid" tooltip-append-to-body="true" tooltip-placement="bottom" style="color: #090;"></span>',
+            '3': '<span class="staticon glyphicon glyphicon-share-alt" tooltip="Recommended" tooltip-append-to-body="true" tooltip-placement="bottom"></span>',
+            '4': '<span class="staticon glyphicon glyphicon-thumbs-up" tooltip="Approved" tooltip-append-to-body="true" tooltip-placement="bottom"></span>',
+            '5': '<span class="staticon glyphicon glyphicon-thumbs-down" tooltip="Denied" tooltip-append-to-body="true" tooltip-placement="bottom" style="color: #900;"></span>',
+            'default': '<span class="staticon glyphicon glyphicon-wrench" tooltip="In_Progress" tooltip-append-to-body="true" tooltip-placement="bottom" style="color: #009;"></span>'
         };
 
         function linker(scope, element, attrs) {
